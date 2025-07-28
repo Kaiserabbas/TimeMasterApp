815dca276a8350a17d0909ed634f8eaeb9f04d79
Qaisar <kayser.abbas@gmail.com>
2025-07-28 14:05:07 +0500

    Update for design change

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'screens/theme_provider.dart';
 import 'screens/home_screen.dart';
 
 void main() {
-  runApp(TimeMasterApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: TimeMasterApp(),
+    ),
+  );
 }
 
 class TimeMasterApp extends StatelessWidget {
