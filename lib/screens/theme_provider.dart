bf62b970cb44c6a20cee9dd7f29fc0e9cd228e83
Qaisar <kayser.abbas@gmail.com>
2025-07-29 15:29:20 +0500

    Update for google fonts

--- lib/screens/theme_provider.dart
+++ lib/screens/theme_provider.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ThemeProvider with ChangeNotifier {
   Color _backgroundColor = Colors.black;
   Color _textColor = Colors.white;
   String _fontFamily = 'Roboto';
+  double _fontSize = 60;
 
   Color get backgroundColor => _backgroundColor;
   Color get textColor => _textColor;
   String get fontFamily => _fontFamily;
+  double get fontSize => _fontSize;
 
   void setBackgroundColor(Color color) {
     _backgroundColor = color;
@@ -24,12 +27,25 @@ class ThemeProvider with ChangeNotifier {
     notifyListeners();
   }
 
-  double _fontSize = 60;
-
-  double get fontSize => _fontSize;
-
   void setFontSize(double size) {
     _fontSize = size;
     notifyListeners();
+  }
+
+  TextStyle getTextStyle() {
+    switch (_fontFamily) {
+      case 'Lato':
+        return GoogleFonts.lato(color: _textColor, fontSize: _fontSize);
+      case 'Open Sans':
+        return GoogleFonts.openSans(color: _textColor, fontSize: _fontSize);
+      case 'Orbitron':
+        return GoogleFonts.orbitron(color: _textColor, fontSize: _fontSize);
+      case 'Anton':
+        return GoogleFonts.anton(color: _textColor, fontSize: _fontSize);
+      case 'Bebas Neue':
+        return GoogleFonts.bebasNeue(color: _textColor, fontSize: _fontSize);
+      default:
+        return GoogleFonts.roboto(color: _textColor, fontSize: _fontSize);
+    }
   }
 }
